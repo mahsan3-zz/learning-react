49106d5683e9daa733ac43e704bee0c05d2f3829
Muhammad Ahsan <mm.ahsan@hotmail.com>
2020-02-09 22:53:58 -0500

    created coffee card component

---
+++ src/CoffeeCard/CoffeeCard.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+class CoffeeCard extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            coffeeObj  : props.coffeeItem,
+            selected: false
+        };
+
+        this.updateName = this.updateName.bind(this);
+    }
+
+    updateName() {
+        this.setState(state => ({
+            selected: !state.selected
+        }));
+    }
+
+    render() {
+        return (
+            <div className="card">
+                <img src={this.state.coffeeObj.image} className="card-img-top" alt="coffee :P" />
+                <div className="card-body">
+                    <h5 className="card-title">{this.state.coffeeObj.name}</h5>
+                    <p className="card-text">
+                        {this.state.coffeeObj.description}
+                    </p>
+                    <button className="btn btn-primary" onClick={this.updateName}>
+                        Like {this.state.selected ? <img src="/icons/check.svg" alt="" width="32" height="32" title="Bootstrap" />: ''}
+
+                    </button>
+                </div>
+            </div>
+        );
+    }
+
+}
+
+export default CoffeeCard;
