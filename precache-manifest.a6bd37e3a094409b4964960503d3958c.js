2bbf1f9c3c934e48ba1a09934029007f134c1fb1
Muhammad Ahsan <mm.ahsan@hotmail.com>
2020-02-09 23:09:18 -0500

    Updates

--- precache-manifest.a6bd37e3a094409b4964960503d3958c.js
+++ precache-manifest.a6bd37e3a094409b4964960503d3958c.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "30375a57640bf912c4c3e7e8bd78dc91",
+    "revision": "960bf73123fbba527905bd7b22530c33",
     "url": "/learning-react/index.html"
   },
   {
-    "revision": "606d32ad1487dc23ef13",
+    "revision": "373e89566db9d7417ed1",
     "url": "/learning-react/static/css/main.bdde9655.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/learning-react/static/js/2.25cbdde4.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "606d32ad1487dc23ef13",
-    "url": "/learning-react/static/js/main.c3b566fa.chunk.js"
+    "revision": "373e89566db9d7417ed1",
+    "url": "/learning-react/static/js/main.dfb87700.chunk.js"
   },
   {
     "revision": "8a52c365d5bd8b0b7d0d",
