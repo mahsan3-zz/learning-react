42f7b9b4e622ab600bf17e42cece238337e97533
Muhammad Ahsan <mm.ahsan@hotmail.com>
2020-02-24 13:37:20 -0500

    Added new state slice to app

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,11 +1,13 @@
 import {combineReducers} from "redux";
 import { combineEpics } from 'redux-observable';
 import appReducer from "./app.reducer";
+import searchReducer from "./securities.reducer";
 
 export const rootEpic = combineEpics(
 
 );
 
 export const rootReducer = combineReducers({
-    appReducer
+    appReducer,
+    searchReducer
 });
