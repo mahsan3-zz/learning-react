458e2f63abf6246e01de89ae4e3531aa2abbba1e
Muhammad Ahsan <mm.ahsan@hotmail.com>
2020-02-25 03:40:18 -0500

    Added action to update search results

--- src/store/actions/securities.actions.js
+++ src/store/actions/securities.actions.js
@@ -1,8 +1,14 @@
 export const SEARCH_SECURITIES = "SEARCH_SECURITIES";
+export const UPDATE_SECURITIES_RESULT = "UPDATE_SECURITIES_RESULT";
 
 export const searchSecurities = q => ({
     type: SEARCH_SECURITIES,
     payload: {
         q
     }
+});
+
+export const updateSecuritiesResult = data => ({
+    type: UPDATE_SECURITIES_RESULT,
+    payload: data
 });
