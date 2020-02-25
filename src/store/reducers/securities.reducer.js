f24500ca77f4702815336235bcc4500f4b074457
Muhammad Ahsan <mm.ahsan@hotmail.com>
2020-02-25 03:41:05 -0500

    Added isLoading flag, and reducers for sending off query and getting results

--- src/store/reducers/securities.reducer.js
+++ src/store/reducers/securities.reducer.js
@@ -1,9 +1,24 @@
+import {SEARCH_SECURITIES, UPDATE_SECURITIES_RESULT} from "../actions/securities.actions";
+
 const initialState = {
+    isLoading: false,
     searchResults: []
 };
 
 export default function searchReducer(state = initialState, action) {
     switch (action.type) {
+        case SEARCH_SECURITIES:
+            return {
+                ...state,
+                searchResults: [],
+                isLoading: true
+            };
+        case UPDATE_SECURITIES_RESULT:
+            return {
+                ...state,
+                isLoading: false,
+                searchResults: action.payload
+            };
         default:
             return state;
     }
