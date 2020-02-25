d6d6f8615a4a608eb845fd410c489df6ad8d1c64
Muhammad Ahsan <mm.ahsan@hotmail.com>
2020-02-25 10:14:16 -0500

    Added action and reducers for getting symbol data and updating it

--- src/store/actions/securities.actions.js
+++ src/store/actions/securities.actions.js
@@ -2,6 +2,8 @@ export const SEARCH_SECURITIES = "SEARCH_SECURITIES";
 export const UPDATE_SECURITIES_RESULT = "UPDATE_SECURITIES_RESULT";
 export const FAVORITE_SYMBOL = "FAVORITE_SYMBOL";
 export const UNFAVORITE_SYMBOL = "UNFAVORITE_SYMBOL";
+export const GET_FAVORITE_SYMBOL_PRICE ="GET_FAVORITE_SYMBOL_PRICE";
+export const UPDATE_SYMBOL_PRICE = "UPDATE_SYMBOL_PRICE";
 
 
 export const searchSecurities = q => ({
@@ -24,4 +26,14 @@ export const favoriteSymbol = sym => ({
 export const unfavoriteSymbol = sym => ({
     type: UNFAVORITE_SYMBOL,
     payload: sym
+});
+
+export const getFavoriteSymbolPrice = sym => ({
+    type: GET_FAVORITE_SYMBOL_PRICE,
+    payload: sym
+});
+
+export const updateSymbolPrice = data => ({
+    type: UPDATE_SYMBOL_PRICE,
+    payload: data
 });
--- src/store/reducers/securities.reducer.js
+++ src/store/reducers/securities.reducer.js
@@ -1,15 +1,27 @@
 import {
-    FAVORITE_SYMBOL,
+    FAVORITE_SYMBOL, GET_FAVORITE_SYMBOL_PRICE,
     SEARCH_SECURITIES,
     UNFAVORITE_SYMBOL,
-    UPDATE_SECURITIES_RESULT
+    UPDATE_SECURITIES_RESULT, UPDATE_SYMBOL_PRICE
 } from "../actions/securities.actions";
 
 const initialState = {
+    symbolQuotes: [],
     favSymbols: [],
     isLoading: false,
     searchResults: []
 };
+function resolveDirectionToColor(change) {
+
+    if(!change) return "black";
+
+    if(change[0] === "-") {
+        return "red";
+    }else {
+        return "green";
+    }
+
+}
 
 export default function searchReducer(state = initialState, action) {
     switch (action.type) {
@@ -35,6 +47,36 @@ export default function searchReducer(state = initialState, action) {
                 ...state,
                 favSymbols: state.favSymbols.filter(s => {
                     if(action.payload !== s) return s;
+                })
+            };
+        case GET_FAVORITE_SYMBOL_PRICE:
+            return {
+              ...state,
+                symbolQuotes: [
+                    ...state.symbolQuotes, {
+                        symbol: action.payload,
+                        isLoading: true,
+                        price: 0,
+                        change: 0,
+                        changeColor: ""
+                    }
+                ]
+            };
+        case UPDATE_SYMBOL_PRICE:
+            return {
+                ...state,
+                symbolQuotes: state.symbolQuotes.map(quote => {
+                    if(quote.symbol === action.payload['01. symbol']) {
+                        return {
+                          ...quote,
+                          isLoading: false,
+                          price: action.payload['05. price'],
+                          change: action.payload['09. change'],
+                          changeColor: resolveDirectionToColor(action.payload['09. change'])
+                        };
+                    }
+
+                    return quote;
                 })
             };
         default:
