63dcf425eb8170258112988e254a535609cfaa00
Muhammad Ahsan <mm.ahsan@hotmail.com>
2020-02-25 10:14:49 -0500

    Updated favorite component to include children components

--- src/Home/containers/FavoriteSecurities.js
+++ src/Home/containers/FavoriteSecurities.js
@@ -1,9 +1,61 @@
-import React from "react";
+import React, {useEffect} from "react";
+import {
+    getFavoriteSymbolPrice,
+} from "../../store/actions/securities.actions";
+import {connect} from "react-redux";
+import QuoteCard from "../components/QuoteCard";
 
-function FavoriteSecurities(propes) {
+function FavoriteSecurities(props) {
+
+    useEffect(() => {
+        props.favSymbols.forEach(symbol => props.getFavPrice(symbol));
+    }, []);
+
     return (
-        <h1>Hail destroyer</h1>
+        <div className="row">
+
+            <div className="col-12">
+
+
+
+            </div>
+
+            <div className="col-12">
+
+                <ul className="list-group">
+                    {props.symbolQuotes.map((quote, i) => {
+                        return (
+                            <QuoteCard key={i}
+                                symbol={quote.symbol}
+                                price={quote.price}
+                                change={quote.change}
+                                changeColor={quote.changeColor}
+                            />
+                        );
+                    })}
+                </ul>
+
+            </div>
+
+        </div>
     );
 }
+
+// Wire up this component to the store
+function mapStateToProps(state) {
+    return {
+        favSymbols: state.searchReducer.favSymbols,
+        symbolQuotes: state.searchReducer.symbolQuotes,
+        isLoading: state.searchReducer.isLoading
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        getFavPrice: s => dispatch(getFavoriteSymbolPrice(s))
+    };
+}
+
+FavoriteSecurities = connect(mapStateToProps, mapDispatchToProps)(FavoriteSecurities);
 
 export default FavoriteSecurities;
