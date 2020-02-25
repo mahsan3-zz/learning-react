71991bb7efea066bb72c0f60856b692a9af50cfd
Muhammad Ahsan <mm.ahsan@hotmail.com>
2020-02-25 03:43:12 -0500

    Added simple input form that emits the value to the parent container

---
+++ src/Home/components/SecuritiesSearchForm.js
@@ -0,0 +1,49 @@
+import React, {useEffect, useRef, useState} from "react";
+import {fromEvent} from "rxjs";
+import PropTypes from 'prop-types'
+
+export default function SecuritiesSearchForm (props) {
+
+    const [searchQuery, setSearchQuery] = useState("");
+    const inputEl = useRef(null);
+
+    useEffect(() => {
+        fromEvent(inputEl.current, 'keyup').subscribe((e) => {
+            console.log(e.key);
+            console.log(e.target.value);
+        });
+
+    }, []);
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log(searchQuery);
+        props.updateSearchResults(searchQuery);
+    };
+
+    return (
+        <form className="" onSubmit={handleSubmit}>
+
+            <div className="form-group">
+                <label htmlFor="searchInput">Search security by name</label>
+
+                <input type="text" className="form-control" id="searchInput"
+                       onChange={e => setSearchQuery(e.target.value)} ref={inputEl} />
+
+                <small id="searchHelp" className="form-text text-muted">
+                    Example: search by company name or stock symbol
+                </small>
+            </div>
+
+            <button type="submit" className="btn btn-primary">
+                Search
+            </button>
+
+        </form>
+    );
+
+}
+
+SecuritiesSearchForm.propType = {
+    updateSearchResults: PropTypes.func.isRequired
+};
