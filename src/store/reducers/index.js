36dd869a95a17a63e5a59c53c0469f537653ebef
Muhammad Ahsan <mm.ahsan@hotmail.com>
2020-02-25 03:45:00 -0500

    Added my first epic

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -2,9 +2,10 @@ import {combineReducers} from "redux";
 import { combineEpics } from 'redux-observable';
 import appReducer from "./app.reducer";
 import searchReducer from "./securities.reducer";
+import {searchSecuritiesEpic} from "../epics/securities.epic";
 
 export const rootEpic = combineEpics(
-
+    searchSecuritiesEpic
 );
 
 export const rootReducer = combineReducers({
